e298c15fd4621426a7228fd66e2f2856b5df3786
blaztprod-cybor <blaztprod@gmail.com>
2026-04-14 22:23:17 -0400

    move to railway

--- server.ts
+++ server.ts
@@ -212,7 +212,7 @@ async function syncPermitFeed() {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = parseInt(process.env.PORT || "3000", 10);
 
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: false }));
